bfe97e02307618ee9eeea300a8a0422b4351a862
RobertoCrofford <roberto.crofford.09@gmail.com>
2021-06-25 21:17:39 -0400

    Coma llamar por id clase etiqueta

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
- alert("hola mundo desde jquery");
+ //alert("hola mundo desde jquery");
+//let varBoton=$('button').html();
+// console.log(varBoton);
+// $('button').html(" cambiando desde query");
+// $('.btn').html(" cambiando desde clase del boton ");//el boton tiene dos clases 
+// $('#boton').html(" cambiando desde desde el ID "); //llamado desde la clase Id 
+// $('button').html(" cambiando desde etiqueta");
+// $('#subcontenedor button').html(" desde subcon");
+   $('button:first').html("primero");
+   $('button:last').html("ultimo");
 });
+
+
+//document.querySelector('#boton').innerHTML=" cambiando desde js";
