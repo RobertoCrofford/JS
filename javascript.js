770f072ba54da08b461af9522518dfd51ee598cc
RobertoCrofford <roberto.crofford.09@gmail.com>
2021-06-26 15:38:34 -0400

    funcionalidades de boton

--- javascript.js
+++ javascript.js
@@ -23,7 +23,29 @@
     $('img').attr({'src':'logo2.jpg', 'width':'400px'});
 });*/
 $(document).ready(function(){
+    $('#boton').click(function (e) { 
+        e.preventDefault();
+        $('img').attr('src', 'logo2.jpg');
+    });
     
+    $('#subcontenedor button:nth-child(1)').click(function (e) { 
+        e.preventDefault();
+        $('img').attr('width', '450px');
+    });
+
+    $('#subcontenedor button:nth-child(2)').click(function (e) { 
+        e.preventDefault();
+        let margen= $('img').css('margin-left');
+        //alerta(margen); 
+        margen=parseInt(margen.replace('px',''));
+        margen=margen+30;
+        $('img').css('margin-left',margen+'px');
+    });
+
+    $('#subcontenedor button:nth-child(3)').click(function (e) { 
+        e.preventDefault();
+        $('img').toggleClass('d-none');
+    });
 });
 
 //document.querySelector('#boton').innerHTML=" cambiando desde js";
